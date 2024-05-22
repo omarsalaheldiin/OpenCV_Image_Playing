8341663699d85ec890ac16b97f3dc6aa02752f6d
omar tamer <153965914+omarsalaheldiin@users.noreply.github.com>
2024-05-22 17:09:22 +0300

    Create colorHistogram.py

---
+++ colorHistogram.py
@@ -0,0 +1,33 @@
+import cv2 as cv
+import matplotlib.pyplot as plt 
+import numpy as np 
+
+def rescale(img, scale):
+    width = int(img.shape[1]*scale)
+    hight = int(img.shape[0]*scale)
+    size = (width, hight)
+    return cv.resize(img, size, interpolation=cv.INTER_CUBIC)
+
+img = rescale(cv.imread("image3.jpg"), 0.35)
+cv.imshow("img", img)
+
+blank = np.zeros(img.shape[:2], dtype='uint8')
+
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+cv.imshow("gray", gray)
+
+gray_hist = cv.calcHist([gray], [0], None, [256], [0,256])
+
+plt.figure()
+plt.title("color histogram")
+plt.xlabel("bins")
+plt.ylabel("# of pixels")
+colors = ('b','g','r')
+for i, col in enumerate(colors):
+    hist = cv.calcHist([img], [i], None, [256], [0,256])
+    plt.plot(hist, color=col)
+    plt.xlim([0,256])
+
+
+plt.show()
+cv.waitKey(0)
